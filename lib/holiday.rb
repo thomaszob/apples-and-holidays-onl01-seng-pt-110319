24d29ef42dcb5d41b2c247e24dc68bb8fd774a8d
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 04:49:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,15 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  items_array = []
+  holiday_hash.each{|season, holiday|
+    if season == :winter
+      holiday.each{|day, items|
+        items_array << items
+      }
+    end 
+  }
+  items_array
 
 end
 

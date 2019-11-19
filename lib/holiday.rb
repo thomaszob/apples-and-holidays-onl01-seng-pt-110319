cf1e0323a44d1c1a63bd0af06d1f2c635563e127
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-19 04:37:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,7 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
   holiday_hash.each{|season, holiday|
     if season == :winter
-      holiday << "supply"
+      holiday << supply
     end 
   }
 end
